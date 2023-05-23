33ea383aac372eb5a405b5495cf24c6e0bf17db6
Bhawesh Agarwal <bhaweshagarwal02@gmail.com>
2023-05-23 16:29:11 +0530

    Ic pin value changing

--- Virtual Lab/Assets/Scripts/ICChange.cs
+++ Virtual Lab/Assets/Scripts/ICChange.cs
@@ -13,19 +13,59 @@ public class ICChange : MonoBehaviour
     private Button changeButton;
     [SerializeField]
     private GameObject ICSelection;
+
+    private ICBase IcBase;
     private void Awake()
     {
         changeButton = GetComponent<Button>();
     }
     private void Start()
     {
-        changeButton.onClick.AddListener(changeIc);
+        changeButton.onClick.AddListener(ChangeIc);
     }
 
-    private void changeIc()
+    private void ChangeIc()
     {
-        ICBase IcBase = SimulatorManager.Instance.IcBase;
+        IcBase = SimulatorManager.Instance.IcBase;
         IcBase.ICSprite.sprite = ic.IcSprite;
+        SetInputAndOutputPins();
+        SetVccAndGndPin();
         ICSelection.SetActive(false);
+    }
+
+    private void SetVccAndGndPin()
+    {
+        // VCC pin
+        int pinNumber = ic.VccPin - 1;
+        ChangePinType(pinNumber, PinType.IcVcc);
+
+
+        //Gnd pin
+        pinNumber = ic.GndPin - 1;
+        ChangePinType(pinNumber, PinType.IcGnd);
+
+    }
+
+    private void SetInputAndOutputPins()
+    {
+        for (int i = 0; i < ic.inputPins.Length; i++)
+        {
+            //input pin
+            int pinNumber = ic.inputPins[i] - 1;
+            ChangePinType(pinNumber, PinType.IcInput);
+
+            //Output pin
+            pinNumber = ic.outputPins[i] - 1;
+            ChangePinType(pinNumber, PinType.IcOutput);
+
+
+        }
+    }
+
+    private void ChangePinType(int PinNumber, PinType type)
+    {
+        PinInfo currentPinInfo = IcBase.Pins[PinNumber].GetComponent<PinConnection>().CurrentPinInfo;
+        currentPinInfo.PinNumber = PinNumber + 1;
+        currentPinInfo.Type = type;
     }
 }
