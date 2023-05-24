caaab383e730a5e3a497c07efb95905cb93f7de2
Bhawesh Agarwal <bhaweshagarwal02@gmail.com>
2023-05-24 12:33:22 +0530

    Re-Arranged Script folder

---
+++ Virtual Lab/Assets/Scripts/IC/ICLogic.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ICLogic : MonoBehaviour
+{
+    private void Awake()
+    {
+        this.enabled = false;
+    }
+}
