08f9498dda879e100cb62508ab0d4cb8a54ba71b
MK-09-coder <110844185+MK-09-coder@users.noreply.github.com>
2023-09-01 17:05:59 +0530

    Updated CurrentStatus

--- Virtual Lab/Assets/Scripts/General/CurrentStatusDisplayer.cs
+++ Virtual Lab/Assets/Scripts/General/CurrentStatusDisplayer.cs
@@ -6,9 +6,8 @@ using UnityEngine;
 public class CurrentStatusDisplayer : MonoBehaviour
 {
     private ValuePropagate valuPropogate;
-    private List<PinController> inputPins;
-    private List<PinController> outputPins;
-    
+    private List<PinController> inputPinswithwire = new();
+    private List<PinController> outputPinswithwire = new();
     private void Awake()
     {
         EventService.Instance.AllValuePropagated += ShowStatus;
@@ -28,11 +27,15 @@ public class CurrentStatusDisplayer : MonoBehaviour
     {
         for(int i = 0; i < valuPropogate.InputPins.Count; i++) 
         { 
-            
+            if(valuPropogate.InputPins[i].Wires.Count>0){
+                inputPinswithwire.Add(valuPropogate.InputPins[i]);
+            }   
         }
         for (int i = 0; i < valuPropogate.OutputPins.Count; i++)
         {
-
+            if(valuPropogate.OutputPins[i].Wires.Count>0){
+                outputPinswithwire.Add(valuPropogate.OutputPins[i]);
+            }   
         }
     }
 }
